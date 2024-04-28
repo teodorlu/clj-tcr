1951fd08d1d3a66b59e831349d625a8aadead48c
Teodor Heggelund <git@teod.eu>
2024-04-28 12:31:48 +0200

    tcr

---
+++ examples/ex2/src/user.clj
@@ -0,0 +1,26 @@
+(ns user
+  (:refer-clojure :exclude [test])
+  (:require
+   babashka.process
+   clj-reload.core
+   cognitect.test-runner))
+
+(defn reload [] (clj-reload.core/reload))
+(defn test [] (cognitect.test-runner/test {}))
+(defn commit []
+  (babashka.process/shell "git add .")
+  (babashka.process/shell "git commit -m working"))
+(defn revert []
+  (babashka.process/shell "git reset --hard HEAD"))
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn tcr
+  "TCR RELOADED: AN IN-PROCESS INTERACTIVE LOOP"
+  []
+  (try
+    (reload)
+    (test)
+    (commit)
+    (catch Exception _
+      (revert)
+      (reload))))
